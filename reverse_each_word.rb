aa42e1cb8e32bba05c31f7691afd9c46e3fd681b
Ruari Day <ruariday@gmail.com>
2020-03-09 17:06:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,13 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split
+  new_sentence.each do |word|
+    
+  end 
+end
+=begin
+def reverse_each_word(sentence)
+  new_sentence = sentence.split
   new_sentence.map.reserve_each
 end
+this works but wont allow it because of the enumerator
+=end
