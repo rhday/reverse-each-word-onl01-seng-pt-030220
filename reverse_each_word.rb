df1580bbb7f944803832d83c1be77604ae9d0f9d
Ruari Day <ruariday@gmail.com>
2020-03-09 17:17:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   reversed_sentence = []
   new_sentence = sentence.split
-  new_sentence.each do |word|
+  new_sentence.collect do |word|
     reversed_sentence << word.reverse
   end
   reversed_sentence.join(" ")
@@ -9,7 +9,7 @@ end
 =begin
 def reverse_each_word(sentence)
   new_sentence = sentence.split
-  new_sentence.map.reserve_each
+  new_sentence.collect.reverse_each
 end
 this works but wont allow it because of the enumerator
 =end
